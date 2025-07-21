c41f32d782157fc7ff457d5c74f5b90aeb09d9e0
TiNYy~ <102255841+TiNYyAPAC@users.noreply.github.com>
2025-07-21 14:40:40 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+import string_operations as s
+sample = "hello World"
+
+print("Original: ", sample)
+print("Reversed: ", s.reverse_string(sample))
+print("Capitalized: ", s.capitalize_string(sample))
+print("Lowercase: ", s.lowercase_string(sample))
+print("Uppercase: ", s.uppercase_string(sample))
