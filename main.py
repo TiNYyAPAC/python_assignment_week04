1382cde77b6b33bc6c8cd42c02148555bc0e1dc9
TiNYy~ <102255841+TiNYyAPAC@users.noreply.github.com>
2025-07-21 15:25:23 +0700

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-import string_operations as s
-sample = "hello World"
-
-print("Original: ", sample)
-print("Reversed: ", s.reverse_string(sample))
-print("Capitalized: ", s.capitalize_string(sample))
-print("Lowercase: ", s.lowercase_string(sample))
-print("Uppercase: ", s.uppercase_string(sample))
