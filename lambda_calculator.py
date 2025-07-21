c9e44c585549a523f26454186257dcdd99ba936d
TiNYy~ <102255841+TiNYyAPAC@users.noreply.github.com>
2025-07-21 15:25:32 +0700

    Delete lambda_calculator.py

--- lambda_calculator.py
+++
@@ -1,9 +0,0 @@
-add = lambda x, y: x + y
-subtract = lambda x, y: x - y
-multiply = lambda x, y: x * y
-divide = lambda x, y: x / y
-
-add(10,5) #15
-subtract(10,5) #5
-multiply(10,5) #50
-divide(10,2) #2
